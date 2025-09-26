79ccd2ae6853df6796893fb29242be0670d9e73c
Vinícius Brevesteky Teixeira <130590509+CheerfulVini@users.noreply.github.com>
2025-09-25 21:45:32 -0300

    Create header.php

---
+++ include/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <link rel="stylesheet" type="text/css" href=<?=$caminhoCss ?>>
+    <meta charset="utf-8" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <title>Dreamfest</title>
+  </head>
+  <body>
+    <header>
+        <h1><a id="titulo" href="<?= $caminhoIndex?>"><?= $titulo?></a></h1>
+    </header>
