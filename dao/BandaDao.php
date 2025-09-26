2ebd55e65e81d14b6e9b196a2da73fc44d0401f2
Vinícius Brevesteky Teixeira <130590509+CheerfulVini@users.noreply.github.com>
2025-09-25 21:49:05 -0300

    Create BandaDao.php

---
+++ dao/BandaDao.php
@@ -0,0 +1,55 @@
+<?php
+require_once("../model/conexao.php");
+class BandaDao {
+    private $con;
+    public function __construct(){
+       $this->con=(new Conexao())->conectar();
+    }
+    function inserir($obj) {
+        $sql = "INSERT INTO banda (id, nome, horario, palco, musicas) VALUES (?, ?, ?, ?, ?)";
+        $stmt = $this->con->prepare($sql);
+        $id=$obj->getId();
+    $nome=$obj->getNome();
+    $horario=$obj->getHorario();
+    $palco=$obj->getPalco();
+    $musicas=$obj->getMusicas();
+
+        $stmt->execute([$id,$nome,$horario,$palco,$musicas]);
+    }
+    function listaGeral(){
+        $sql = "select * from banda";
+        $query = $this->con->query($sql);
+        $dados = $query->fetchAll(PDO::FETCH_ASSOC);
+        return $dados;
+    }
+    function excluir($id){
+        $sql = "delete from banda where id = $id";
+        $query = $this->con->query($sql);
+        header("Location:../view/listaBanda.php");
+    }
+    function buscar($id){
+        $sql = "SELECT * FROM banda WHERE id = :id";
+        $stm = $this->con->prepare($sql);
+        $stm->bindValue(":id", $id, PDO::PARAM_INT);
+        $stm->execute();
+
+        return $stm->fetch(PDO::FETCH_ASSOC); 
+    }
+    function alterar($banda){
+        $sql = "UPDATE banda SET nome = ?, horario = ?, palco = ?, musicas = ? WHERE id = ?";
+        $stm = $this->con->prepare($sql);
+        $stm->execute([$banda->getNome(), $banda->getHorario(), $banda->getPalco(), $banda->getMusicas(), $banda->getId()]);
+        return NULL;
+    }
+
+    function getIntegrantes($id_banda){
+    $sql = "SELECT m.id, m.nome, m.nome_artistico, m.instrumento, m.cpf, m.id_banda
+            FROM musico m
+            WHERE m.id_banda = ?";
+
+    $stm = $this->con->prepare($sql);
+    $stm->execute([$id_banda]);
+
+    return $stm->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
