5ed4522159006b7fc7bb3c915ec990284745b501
Vinícius Brevesteky Teixeira <130590509+CheerfulVini@users.noreply.github.com>
2025-11-28 14:54:22 -0300

    Update cardBanda.php

--- view/cardBanda.php
+++ view/cardBanda.php
@@ -14,10 +14,13 @@ $musicos = $dao->getIntegrantes($_GET['id']);
 
 <main>
 
-<div class="card">
+<label for="nome">Buscar Banda</label>
+<input type="text" name="nome" id="nome" onblur="buscaBanda(this.value)">
+<div id="card">
     <h1><?=$banda['nome']?></h1>
     <div class="divisor"><strong>Horario:</strong><p><?=$banda['horario']?><br></p></div>
     <div class="divisor"><strong>Palco:</strong><p><?=$banda['palco']?><br></p></div>
+    <div class="divisor"><strong>Genero:</strong><p><?=$banda['genero']?><br></p></div>
     <div class="divisor"><strong>Musicas:</strong></div>
     <p><?=str_replace(";", "<br>", ($banda['musicas']))?><br></p>
     <div class="divisor"><strong>Integrantes</strong></div>
@@ -31,6 +34,8 @@ $musicos = $dao->getIntegrantes($_GET['id']);
 </div>
 
 </main>
+
+<script src="../js/script.js"></script>
 
 <?php
 include_once("../include/footer.php");
